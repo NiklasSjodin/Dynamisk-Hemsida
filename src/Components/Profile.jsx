a7a8822e78d8e61a6ff772d8f92f0100aae95b17
Niklas Sjödin <146171251+NiklasSjodin@users.noreply.github.com>
2024-04-05 14:50:19 +0200

    Target _blank didnt work

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -28,7 +28,7 @@ export default function Profile() {
           <h1 className="title">Niklas Sjödin</h1>
           <p className="section__text_p2">Fullstack Developer</p>
           <div className="btn-container">
-            <NavLink to="/resume" target="_blank">
+            <NavLink to="/resume">
               <button className="btn btn-color-2" /* onClick={openResume} */>
                 CV
               </button>
