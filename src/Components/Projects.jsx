f62752272e3c97f26d9f1b0b57043520f66d1717
Niklas Sjödin <146171251+NiklasSjodin@users.noreply.github.com>
2024-04-05 18:32:09 +0200

    Adding modal popups in projects section

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -3,9 +3,22 @@ import BankNet from "../assets/banknet-pic.png";
 import Api from "../assets/api-pic.png";
 import Arrow from "../assets/arrow.png";
 
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 import { Link } from "react-scroll";
 
 export default function Projects() {
+  const [show, setShow] = useState(false);
+  const [show1, setShow1] = useState(false);
+  const [show2, setShow2] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const handleClose1 = () => setShow1(false);
+  const handleShow1 = () => setShow1(true);
+  const handleClose2 = () => setShow2(false);
+  const handleShow2 = () => setShow2(true);
   return (
     <>
       <section id="projects">
@@ -28,6 +41,12 @@ export default function Projects() {
                     Github
                   </button>
                 </a>
+                <Button
+                  variant="btn btn-color-2 project-btn"
+                  onClick={handleShow}
+                >
+                  More Info
+                </Button>
               </div>
             </div>
             <div className="details-container color-container">
@@ -45,6 +64,12 @@ export default function Projects() {
                     Github
                   </button>
                 </a>
+                <Button
+                  variant="btn btn-color-2 project-btn"
+                  onClick={handleShow1}
+                >
+                  More Info
+                </Button>
               </div>
             </div>
             <div className="details-container color-container">
@@ -62,6 +87,12 @@ export default function Projects() {
                     Github
                   </button>
                 </a>
+                <Button
+                  variant="btn btn-color-2 project-btn"
+                  onClick={handleShow2}
+                >
+                  More Info
+                </Button>
               </div>
             </div>
           </div>
@@ -70,6 +101,53 @@ export default function Projects() {
           <img src={Arrow} alt="Arrow icon" className="icon arrow" />
         </Link>
       </section>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title className="title">Netify</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="section__text_p1">
+          Welcome to the Netify Api. This projects involves creating a minimal
+          API around the Spotify open access api.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="btn btn-color-2 project-btn" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={show1} onHide={handleClose1}>
+        <Modal.Header closeButton>
+          <Modal.Title className="title">BankNet</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="section__text_p1">
+          Welcome to BankNET! This project involves creating a user-friendly
+          banking system, like an ATM or internet bank. Its a challenging
+          project where well-structured code is essential to avoid confusion.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="btn btn-color-2 project-btn" onClick={handleClose1}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={show2} onHide={handleClose2}>
+        <Modal.Header closeButton>
+          <Modal.Title className="title">Mini API</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="section__text_p1">
+          This project aims to develop a system for storing individuals
+          essential details like names and phone numbers. It supports the
+          storage of multiple interests per person, each having a title and
+          brief description. Individuals can be connected to various interests,
+          and multiple website links can be associated with both individuals and
+          their interests
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="btn btn-color-2 project-btn" onClick={handleClose2}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
