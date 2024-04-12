25b81285cfc9bc5a337cc576f4a947b9b72e89b3
Niklas Sjödin <146171251+NiklasSjodin@users.noreply.github.com>
2024-04-12 10:52:07 +0200

    Change Link in Hamburger section to "react-scroll"

--- src/Components/Hamburger.jsx
+++ src/Components/Hamburger.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link } from "react-scroll";
 
 export default function Hamburger() {
   const [isOpen, setIsOpen] = useState(false);
